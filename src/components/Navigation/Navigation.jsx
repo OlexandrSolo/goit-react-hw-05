01919c0e266db2fd461267990199acc447f0f4d9
Your Name <you@example.com>
2024-09-09 12:36:05 +0300

    started stelazation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,22 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import style from "../Navigation/Navigation.module.css";
+
+const getNavLinkClass = (props) => {
+  return clsx(style.link, props.isActive && style.active);
+};
 
 export default function Navigation() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-    </nav>
+    <header className={style.container}>
+      <nav>
+        <NavLink to="/" className={getNavLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={getNavLinkClass}>
+          Movies
+        </NavLink>
+      </nav>
+    </header>
   );
 }
